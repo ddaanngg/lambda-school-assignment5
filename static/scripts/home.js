04fe7a20f471bbba3d77bf0e54be278cb6a2721a
David Dang <daviddang@Davids-MacBook-Pro.local>
2017-02-27 18:16:08 -0800

    Completed assignment including extra credit.

--- static/scripts/home.js
+++ static/scripts/home.js
@@ -2,18 +2,40 @@
 
 function validateForm() {
   var name = document.getElementById('nameInput').value;
-  species = document.getElementById('speciesInput').value;
-  age = document.getElementById('ageInput').value;
+  var species = document.getElementById('speciesInput').value;
+  var age = document.getElementById('ageInput').value;
+  console.log(name, species, age);
 
   if(!name.length || !species.length || !age.length) {
     alert('Fields must not be blank');
     console.log(name, species, age);
+    return false;
   }
 
-  if(typeof parseInt(age) !== 'number') {
+  if(typeof name !== 'string' || typeof species !== 'string') {
+    alert('Name and species must be alphabetical');
+    console.log(typeof name, typeof species);
+    return false;
+  }
+
+  if(isNaN(parseInt(age)) || typeof parseInt(age) !== 'number') {
     alert('Age must be a number');
-    console.log(typeof(age));
+    console.log(typeof parseInt(age));
+    return false;
   }
 
-  return false;
+  return true;
 }
+
+// change text color javascript
+document.getElementById('changeTextColor').addEventListener('click', function() {
+  var color = document.getElementById('textColorInput').value;
+  document.body.style.color = color;
+  console.log('color', color);
+  console.log('You clicked me');
+});
+
+// hide display javascript
+document.getElementById('hideDisplay').addEventListener('click', function() {
+  document.body.style.display = "none";
+});
